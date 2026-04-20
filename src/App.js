611ee33565edc3391d23e64a83d5224365c7c42f
GiovannyPadilla <giovanny.padilla@udla.edu.ec>
2026-04-19 20:11:41 -0500

    fix:App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API = "http://localhost:8080";
+const API = "https://minicoreback-1.onrender.com";
 
 const fmt = (v) =>
     new Intl.NumberFormat("es-EC", { style: "currency", currency: "USD" }).format(v);
